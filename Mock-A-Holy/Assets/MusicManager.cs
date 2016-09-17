a4a521ec79917abbd7786bd9edcc682055d557d8
Lorenzo Salvadori <lorenzo.salvadori.86@gmail.com>
2016-09-17 13:52:18 +0200

    [Audio] Added FMOD components to Unity - added fmod listener to MainCamera in MainMenu and MainScence scenes - created and added MusicManager.cs ad added it to MainCamera in MainMenu and MainScene scenes

---
+++ Mock-A-Holy/Assets/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using FMODUnity;
+
+public class MusicManager : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string MUS_MainMenuEvent;
+    FMOD.Studio.EventInstance MUS_MainMenu;
+    public string p_MenuState;
+    public string p_AdeptStatus;
+    public string p_Thunderstruck;
+
+    // float value;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        MUS_MainMenu = FMODUnity.RuntimeManager.CreateInstance(MUS_MainMenuEvent);
+        MUS_MainMenu.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    //    MUS_MainMenu.setParameterValue(p_MenuState, value);
+    }
+}
